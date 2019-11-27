5de8d3a0f2fdeff57079ecb1c291a18a9e068538
GryMich <GryMich@gmail.com>
2019-11-27 21:40:29 +0100

    exc36

--- src/exc36o4CountingAverage.java
+++ src/exc36o4CountingAverage.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class exc36o4CountingAverage {
-
+//jest to ca³e zadanie 36
 	public static void main(String[] args) {
 		
 		Scanner reader = new Scanner (System.in);
@@ -12,6 +12,8 @@ public class exc36o4CountingAverage {
 		int sum = 0;
 		int counter = 0;
 		double average = 0.0;
+		int even = 0;
+		int odd = 0;
 		
 		while (number >= 0) {
 			
@@ -21,19 +23,23 @@ public class exc36o4CountingAverage {
 			if (number == -1) {
 				
 				break; 
+			} if (number % 2 == 0) {
+				even++;
 				
-			} else  {
-				
-				sum = sum + number;
-				counter++;
-				
-								
+			} if (number % 2 != 0) {
+				odd++;
+																								
 			}
+			
+			sum = sum + number;
+			counter++;
 		}	
 		System.out.println("Thank you!");
 		System.out.println("The sum is: " + sum);
 		System.out.println("How many numbers: "+ counter);
 		System.out.println("Average: "+ (double)sum/counter);
+		System.out.println("Even numbers: "+ even);
+		System.out.println("Odd numbers: "+ odd);
+		}
+		
 	}
-
-}
