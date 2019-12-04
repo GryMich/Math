2bbc174da1ddb5c1e3aeb6f8038dadb4931ef73d
GryMich <GryMich@gmail.com>
2019-12-04 13:07:57 +0100

    MethodsPrinting

---
+++ src/exc39MethodsPrinting.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class exc39MethodsPrinting {
+	
+	private static void printTriangle (int size) {
+		
+		int i = 0;
+		int a = 1;
+		
+		while (size>i && a<=size) {
+			
+			printStars(a++);
+			
+			i++;
+		}
+	}
+	private static void printRectangle(int width, int heigth) {
+		
+		int i = 0;
+		
+		while (width>=i && heigth>i) {
+			
+			printStars (width);
+			
+			i++;
+			
+			
+		}
+		
+	}
+	private static void printSquare (int sideSize) {
+		
+		int i = 0;
+		
+		while (i<sideSize) {
+			
+			printStars(4);
+			i++;
+			
+			if (i==sideSize) {
+						break;
+			}
+		}
+	}
+	private static void printStars (int amount) {
+			
+		int i = 0;
+		
+		while (i<amount) {
+			
+			System.out.print("*");
+			i++;
+			
+			
+		}
+		
+		System.out.println();	
+	}
+
+	public static void main(String[] args) {
+
+	printTriangle(4);
+	
+	}
+
+}
